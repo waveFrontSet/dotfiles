95e23a5ece108762aed8dacbc68931609fddccaa
Paul Grillenberger <pgrillenberger@nexible.de>
2021-03-02 17:24:49 +0100

    Add javascript

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -123,7 +123,8 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript        ; all(hope(abandon(ye(who(enter(here))))))
+        +lsp)
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex             ; writing papers in Emacs has never been so fun
