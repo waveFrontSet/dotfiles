1667f56a995ab82d906e738421eaf9a4a039b600
Paul Grillenberger <grillenbergerpaul@gmail.com>
2026-03-29 22:51:33 +0200

    Switch to tokyonight-storm colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
   {
+    "folke/tokyonight.nvim",
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "tokyonight-storm",
     },
   },
 }
