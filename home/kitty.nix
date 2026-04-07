2924da21cfcc4fdc7486aaf8e080bb56efde985a
Paul Grillenberger <grillenbergerpaul@gmail.com>
2026-04-08 00:27:30 +0200

    Move kitty config to nix

--- home/kitty.nix
+++ home/kitty.nix
@@ -1,12 +1,26 @@
 # Home-manager configuration for kitty
-{ dotfiles, ... }:
+{ ... }:
 {
-  home.file = {
-    ".config/kitty/diff.conf".source = "${dotfiles}/kitty/.config/kitty/diff.conf";
-    ".config/kitty/current-theme.conf".source = "${dotfiles}/kitty/.config/kitty/current-theme.conf";
-  };
   programs.kitty = {
     enable = true;
-    extraConfig = builtins.readFile (dotfiles + "/kitty/.config/kitty/kitty.conf");
+    enableGitIntegration = true;
+    font = {
+      name = "family=\"Meslo LG S\"";
+      size = 16.0;
+    };
+    themeFile = "tokyo_night_storm";
+    settings = {
+      hide_window_decorations = "yes";
+      tab_bar_edge = "top";
+      tab_bar_margin_width = 0.0;
+      tab_bar_style = "powerline";
+      tab_powerline_style = "round";
+      tab_bar_min_tabs = 1;
+      tab_activity_symbol = "⚠️";
+      macos_option_as_alt = "left";
+      macos_thicken_font = 0.0;
+      scrollback_lines = 5000;
+      scrollback_pager = "less --chop-long-lines --RAW-CONTROL-CHARS +INPUT_LINE_NUMBER";
+    };
   };
 }
