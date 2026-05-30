6135268ff43eef86a9088fdc4245c5d04b99a365
Paul Grillenberger <grillenbergerpaul@gmail.com>
2026-05-30 11:44:05 +0200

    Update talos

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,12 +1,12 @@
 final: prev: {
   talosctl = prev.talosctl.overrideAttrs (old: rec {
-    version = "1.12.7";
+    version = "1.12.8";
     src = final.fetchFromGitHub {
       owner = "siderolabs";
       repo = "talos";
       tag = "v${version}";
-      hash = "sha256-8RiEiOqWuFcE/cFUjm9EyC3HCK3QTFjSBudTW05XtdQ=";
+      hash = "sha256-EqeohwiE5f1ka1l+gAtM5jZ5KjTQy0oV4JH4Alaie7A=";
     };
-    vendorHash = "sha256-6n3h/RAp25IjFjVQ4g6tCJi6sEvKtj59BHdjdCnDl7E=";
+    vendorHash = "sha256-9J1XcBJrJiiEMEvwHtHdbZkLSLJaRQcpICNE3hcCwz8=";
   });
 }
